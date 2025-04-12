632eafccdc10c9385c994a591aaa6c721f415f8f
Gb7775 <gwstore.oficial00@gmail.com>
2025-04-11 23:37:20 -0200

    Update produto.js

--- produto.js
+++ produto.js
@@ -3,6 +3,6 @@ const nome = params.get('nome');
 const img = params.get('img');
 const desc = params.get('desc');
 
-document.getElementById('nome-produto').innerText = nome;
+document.getElementById('dedeira-premium').innerText = nome;
 document.getElementById('img-produto').src = img;
 document.getElementById('desc-produto').innerText = desc;
