7933c79cbe8732907f072549914b86fa4c5db32a
Gb7775 <gwstore.oficial00@gmail.com>
2025-04-12 23:06:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// Função para abrir a página de produto correspondente
-function abrirProduto(nomeProduto, imagemProduto) {
-  // Armazena os dados no localStorage
-  localStorage.setItem("produtoNome", nomeProduto);
-  localStorage.setItem("produtoImagem", imagemProduto);
-
-  // Redireciona para a página do produto
-  window.location.href = "https://raw.githubusercontent.com/Gb7775/Gwstore/refs/heads/main/produto1.html";
-}
-
-// Função do menu hamburguer
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("show");
+function abrirProduto(produtoNome, produtoImagem) {
+  // Salvar o nome e a imagem do produto no localStorage
+  localStorage.setItem("produtoNome", produtoNome);
+  localStorage.setItem("produtoImagem", produtoImagem);
+  
+  // Redirecionar para a página do produto
+  window.location.href = produtoNome.toLowerCase().replace(" ", "") + ".html";
 }
