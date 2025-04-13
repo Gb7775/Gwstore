1e2edb63e4c757591eca0e9c5b517a6115a47876
Gb7775 <gwstore.oficial00@gmail.com>
2025-04-12 23:52:34 -0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function toggleMenu() {
 }
 
 function abrirProduto1() {
-  window.location.href = "produto.html?nome=Produto 1&img=https://via.placeholder.com/400x400";
+  window.location.href = "https://raw.githubusercontent.com/Gb7775/Gwstore/refs/heads/main/produto1.htmlimg=https://via.placeholder.com/400x400";
 }
 
 function abrirProduto2() {
